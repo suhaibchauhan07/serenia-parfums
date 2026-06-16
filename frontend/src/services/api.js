0215828e56e2f6c454f3df98951db68e1784303f
suhaib chauhan <chauhansuhaib07@gmail.com>
2026-06-16 23:02:53 +0530

    Update API URL to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://serenia-parfums-backend.onrender.com/api',
 });
 
 // Add a request interceptor to add the token to every request
